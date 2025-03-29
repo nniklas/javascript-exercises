775c07da0e3b9262b237fa7bad33be01b7221b84
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-29 13:25:22 +0100

    complete repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeat) {
+    if (numRepeat < 0){
+        return "ERROR"
+    }
+    let returnString = "";
+    for (let i = 0; i < numRepeat; i++){
+        returnString += string
+    }
+    return returnString
 };
 
 // Do not edit below this line
