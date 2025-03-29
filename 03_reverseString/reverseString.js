9fbb3d80a9b1d225f50b7084e21bcffb3dd87b6d
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-29 22:13:25 +0100

    complete exercise 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split("");
+    wordArray.reverse();
+    reversedString = wordArray.join("");
+    return reversedString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removeThis) {
+    for (let i = 0; i<array.length;i++){
+        for (j=0; j < removeThis.length; j++){
+            if (array[i] === removeThis[j]){
+                array.splice(i)
+                console.log(array)
+            }   
+        }
+    }
+    return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
