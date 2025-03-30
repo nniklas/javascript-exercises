9cc3001f6f6faebb9cc8af2d028f754da90ddbf8
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-30 10:41:19 +0200

    fix work with large number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    let startNumber = 0;
+    let endNumber = 0;
+    // check which is startnumber and endnumber
+    if (num1 < num2){
+        startNumber = num1;
+        endNumber = num2;
+    } else {
+        startNumber = num2;
+        endNumber = num1;
+    }
 
+    // check that numbers are positive integers. if string make them integers
+
+    // Start a loop from the startnumber, to the endnumber, index increasing 1
+    // for each loop, add the current number to the sum
+    for (let i = startNumber; i <= endNumber; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
