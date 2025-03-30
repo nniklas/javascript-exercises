cf71220dd826cfba18d7b79201122fa03a103cfd
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-30 11:29:28 +0200

    complete ex 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // x-32*5/9
+  let celsius = (fahrenheit-32)*5/9;
+  //round to 1 decimal
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // x *9/5+32
+  let fahrenheit = celsius * 9/5 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit
+
 };
 
 // Do not edit below this line
