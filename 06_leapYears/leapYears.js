b70f4c8308f971d3f6c25b736a386202afe13921
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-30 11:19:55 +0200

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // shall be divisible by four
+    // shall not be divisible by 100, unless its 400 then its ok
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        return true
+    } else {
+        return false
+    } 
 };
 
 // Do not edit below this line
