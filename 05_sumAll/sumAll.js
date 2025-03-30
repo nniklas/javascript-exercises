a9908f6f66c4b929f59e50ae522b26330fcaf401
nniklas <46904141+nniklas@users.noreply.github.com>
2025-03-30 10:46:37 +0200

    fix error if number is not integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,7 @@ const sumAll = function(num1,num2) {
     let startNumber = 0;
     let endNumber = 0;
     // check which is startnumber and endnumber
-    if (num1 < 0 || num2 < 0){
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
         return "ERROR"
     } else if (num1 < num2){
         startNumber = num1;
