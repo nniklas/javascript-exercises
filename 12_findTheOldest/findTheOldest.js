b5800b6430f3e10f396443604a2d7ecd4293b4aa
nniklas <46904141+nniklas@users.noreply.github.com>
2025-04-13 21:22:39 +0200

    complete ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentDate = new Date().getFullYear()
 
+    let ages = people.map(
+        el => {
+            if ("yearOfDeath" in el){
+                console.log(`current ${ currentDate}, yearofdeath ${el.yearOfDeath}, yearof 
+                birth ${el.yearOfBirth} `)
+                return el.yearOfDeath - el.yearOfBirth
+            } else{
+                console.log(" NO yearofDEATH !")
+                console.log(`current ${ currentDate}, yearofdeath ${el.yearOfDeath}, yearof 
+                    birth ${el.yearOfBirth} `)
+                return currentDate - el.yearOfBirth;
+                
+            }});
+
+    let i = ages.indexOf(Math.max(...ages));
+    console.log(ages)
+    return people[i]
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
