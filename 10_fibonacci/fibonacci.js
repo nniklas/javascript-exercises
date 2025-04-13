987f545051ed99b38cca2de484314103b2c1eb79
nniklas <46904141+nniklas@users.noreply.github.com>
2025-04-13 20:49:57 +0200

    complete ex 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    if (num == 0){
+        return 0
+    } else if (num < 0){
+        return "OOPS"
+    }
+    const series = [1,1];
+    // console.log(series);
+    let nextFib = 0; 
+    for (i=1; i < num; i++){
+        nextFib = series[i-1] + series[i];
+        series.push(nextFib);
+        // console.log(`series[i-1] ${series[i-1]}, series[i-1] ${series[i]}, series ${series}
+        //     , nextfib ${nextFib}`);
+    };
 
+    return series[num-1]
 };
 
 // Do not edit below this line
